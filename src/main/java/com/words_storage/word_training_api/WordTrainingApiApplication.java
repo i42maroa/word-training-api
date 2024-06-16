c6b589d25cf13a7d8a812d30ab6906222a81590e
Antonio Marín <amrodriguezjarote@gmail.com>
2024-06-16 17:33:36 +0200

    first commit

---
+++ src/main/java/com/words_storage/word_training_api/WordTrainingApiApplication.java
@@ -0,0 +1,13 @@
+package com.words_storage.word_training_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordTrainingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordTrainingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/words_storage/word_training_api/WordTrainingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.words_storage.word_training_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordTrainingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
