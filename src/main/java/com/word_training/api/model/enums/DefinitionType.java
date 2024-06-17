105abdc85aae0d6d21078bcf42726127d64d4fe7
Antonio Marín <amrodriguezjarote@gmail.com>
2024-06-17 16:07:42 +0200

    Wip: define controllers and make services

--- src/main/java/com/word_training/api/model/enums/DefinitionType.java
+++ src/main/java/com/word_training/api/model/enums/DefinitionType.java
@@ -1,11 +1,32 @@
 package com.word_training.api.model.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
 @Getter
+@AllArgsConstructor
 public enum DefinitionType {
-    NOUN,
-    ADJ,
-    ADV,
-    VERB
+    NOUN("noun"),
+    ADJ("adjective"),
+    ADV("adverb"),
+    VERB("verb");
+
+    private final String value;
+    private static final Map<String, DefinitionType> typeMap = new HashMap<>();
+
+    static {
+        for (DefinitionType type: DefinitionType.values()){
+            typeMap.put(type.getValue(), type);
+        }
+    }
+
+    public static DefinitionType getDefinitionType(String type) {
+        return typeMap.get(type);
+    }
+
+    public static final Predicate<String> isValidDefinitionType = typeMap::containsKey;
 }
